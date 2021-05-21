8c2748d26502b45071ef69eed11ea68b1c9c8200
PhamHuynhThanhLam <43400884+PhamHuynhThanhLam@users.noreply.github.com>
2021-05-21 10:28:46 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+##libraries
+from flask import Flask, jsonify, request
+import data
+import utils
+
+# Khởi tạo model.
+global model 
+model = []
+count_matrix = []
+cosine_sim = []
+# Khởi tạo flask app
+app = Flask(__name__)
+
+# Khai báo các route 1 cho API
+@app.route("/", methods=["GET"])
+# Khai báo hàm xử lý dữ liệu.
+def _hello_world():
+	return "Hello, Flask!"
+
+@app.route("/getId", methods=["POST"])
+def getID():
+    id = request.form["id"]
+    model = data.requre(id)
+    if len(model) != 0:
+        count_matrix,cosine_sim = utils.count(model)
+        indices = []
+        for i in range(0, len(model), 1):
+            indices.append(model[i]['Title'])
+
+    return jsonify(utils.recommend('Phòng cho thuê mới 100% tại Cần Thơ',cosine_sim, indices, model))
+
+if __name__ == '__main__':
+    app.run(debug=True,threaded=True, port=5000)
+    
+
