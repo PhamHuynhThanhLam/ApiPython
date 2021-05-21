5feb69c0adeeba337ce36f870038edd1ac8c0dbc
PhamHuynhThanhLam <43400884+PhamHuynhThanhLam@users.noreply.github.com>
2021-05-21 16:35:58 +0700

    new

--- app.py
+++ app.py
@@ -12,22 +12,33 @@ cosine_sim = []
 app = Flask(__name__)
 
 # Khai báo các route 1 cho API
-@app.route("/", methods=["GET"])
+@app.route('/', methods=['GET','POST'])
 # Khai báo hàm xử lý dữ liệu.
 def _hello_world():
-	return "Hello, Flask!"
+    if request.method == 'POST':
+        id = request.form["id"]
+        model = data.requre(id)
+        if len(model) != 0:
+            count_matrix,cosine_sim = utils.count(model)
+            indices = []
+            for i in range(0, len(model), 1):
+                indices.append(model[i]['Title'])
 
-@app.route("/getId", methods=["POST"])
-def getID():
-    id = request.form["id"]
-    model = data.requre(id)
-    if len(model) != 0:
-        count_matrix,cosine_sim = utils.count(model)
-        indices = []
-        for i in range(0, len(model), 1):
-            indices.append(model[i]['Title'])
+        return jsonify(utils.recommend('Phòng cho thuê mới 100% tại Cần Thơ',cosine_sim, indices, model))
+    else :
+	    return "Hello, Flask!"
 
-    return jsonify(utils.recommend('Phòng cho thuê mới 100% tại Cần Thơ',cosine_sim, indices, model))
+# @app.route('/getId', methods=['POST'])
+# def getID():
+#     id = request.form["id"]
+#     model = data.requre(id)
+#     if len(model) != 0:
+#         count_matrix,cosine_sim = utils.count(model)
+#         indices = []
+#         for i in range(0, len(model), 1):
+#             indices.append(model[i]['Title'])
+
+#     return jsonify(utils.recommend('Phòng cho thuê mới 100% tại Cần Thơ',cosine_sim, indices, model))
 
 if __name__ == '__main__':
     app.run(debug=True,threaded=True, port=5000)
